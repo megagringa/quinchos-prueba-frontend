7916432553c5fee87318ea9b832b0a458058aa9d
Juan Pablo Lopez <38148731+megagringa@users.noreply.github.com>
2023-10-29 02:51:01 -0300

    add react();

---
+++ mercado-quincho-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'; // Asegúrate de tener un archivo CSS para estilizar tu página
+
+function App() {
+  return (
+    <div className="App">
+ 
+      <footer>
+        <p>Derechos de autor © 2023</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
